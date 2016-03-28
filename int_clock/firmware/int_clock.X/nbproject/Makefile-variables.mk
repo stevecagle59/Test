9959c0ce9709327f4676e5b3d48a75bf2dbfac6a
stevecagle59 <stevecagle59@yahoo.com>
2016-03-28 13:03:18 -0400

    Added files via upload
    A few files for testing

---
+++ int_clock/firmware/int_clock.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# int_clock configuration
+CND_ARTIFACT_DIR_int_clock=dist/int_clock/production
+CND_ARTIFACT_NAME_int_clock=int_clock.X.production.hex
+CND_ARTIFACT_PATH_int_clock=dist/int_clock/production/int_clock.X.production.hex
+CND_PACKAGE_DIR_int_clock=${CND_DISTDIR}/int_clock/package
+CND_PACKAGE_NAME_int_clock=intclock.x.tar
+CND_PACKAGE_PATH_int_clock=${CND_DISTDIR}/int_clock/package/intclock.x.tar
